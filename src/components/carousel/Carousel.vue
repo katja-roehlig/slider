f18400ba751d9434e3306f62ac814f62eba630c8
Katja Roehlig <katja-roehlig@mail.de>
2023-06-05 12:53:11 +0100

    feat: set slide-show

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,12 +1,57 @@
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import CarouselItem from './CarouselItem.vue'
+
 const props = defineProps({ slides: Object })
+console.log(props.slides)
+let currentSlide = ref(0)
+let slideInterval = ref(null)
+
+onMounted(() => {
+  slideInterval = setInterval(() => {
+    const index = currentSlide.value < props.slides.length - 1 ? currentSlide.value + 1 : 0
+    /* let index = 0
+    if (currentSlide.value < props.slides.length - 1) {
+      index = currentSlide.value + 1
+    } else {
+      index = 0
+    } */
+    setCurrentSlide(index)
+  }, 3000)
+})
+console.log(slideInterval)
+
+onBeforeUnmount(() => {
+  clearInterval(slideInterval)
+})
+
+function setCurrentSlide(index) {
+  currentSlide.value = index
+}
 </script>
 
 <template>
   <div class="carousel">
     <div class="carousel-inner">
-      <CarouselItem v-for="(slide, index) in slides" :slide="slide" :key="`item-${index}`" />
+      <CarouselItem
+        v-for="(slide, index) in slides"
+        :slide="slide"
+        :key="`item-${index}`"
+        :current-slide="currentSlide"
+        :index="index"
+      />
     </div>
   </div>
 </template>
+<style scoped>
+.carousel {
+  display: flex;
+  justify-content: center;
+}
+.carousel-inner {
+  position: relative;
+  width: 900px;
+  height: 400px;
+  overflow: hidden;
+}
+</style>
--- src/components/carousel/CarouselItem.vue
+++ src/components/carousel/CarouselItem.vue
@@ -1,9 +1,19 @@
 <script setup>
-const props = defineProps({ slide: String })
+const props = defineProps({ slide: String, currentSlide: Number, index: Number })
 </script>
 
 <template>
-  <div class="carousel-item">
-    <img :src="slide" />
+  <div class="carousel-item" v-show="props.currentSlide === props.index">
+    <img :src="props.slide" />
   </div>
 </template>
+
+<style scoped>
+.carousel-item {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
